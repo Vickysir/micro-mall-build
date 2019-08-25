3059db2a7e7c9ea120cfa8b295b5e1176a150dbc
vickyYu <ziwei.yu@boonray.com>
2019-08-25 14:21:17 +0800

    init

---
+++ build/precache-manifest.fc8dc0a70b5849f6962d84c4cbe2925a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651ce90259941ced52f137a707f59873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ad1646a6363ebd5aacc",
+    "url": "/static/css/2.ffe2c503.chunk.css"
+  },
+  {
+    "revision": "49448e370e5e26980d7d",
+    "url": "/static/css/main.866a7cba.chunk.css"
+  },
+  {
+    "revision": "4ad1646a6363ebd5aacc",
+    "url": "/static/js/2.e9d8fe38.chunk.js"
+  },
+  {
+    "revision": "49448e370e5e26980d7d",
+    "url": "/static/js/main.c391e206.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6f28383b978b6a9b02744766b8eaa6e7",
+    "url": "/static/media/boy.6f28383b.svg"
+  },
+  {
+    "revision": "b5b1bbda19fae57b04dbc6b063e242a6",
+    "url": "/static/media/fullreduction.b5b1bbda.svg"
+  },
+  {
+    "revision": "2d756bfcf2dda8f9c855f82d7d82a709",
+    "url": "/static/media/noImg.2d756bfc.svg"
+  },
+  {
+    "revision": "b8ae93b57afaa41f64a6b0ba4ba277cf",
+    "url": "/static/media/order.b8ae93b5.svg"
+  },
+  {
+    "revision": "b485ba8ee780ecaf17d6241d08688558",
+    "url": "/static/media/五金工具.b485ba8e.svg"
+  },
+  {
+    "revision": "7a17d420e2521adf5f7312dcdfacfcdf",
+    "url": "/static/media/待付款.7a17d420.svg"
+  },
+  {
+    "revision": "21b150569e04f1ccc1691d8f8395bd6c",
+    "url": "/static/media/待收货.21b15056.svg"
+  },
+  {
+    "revision": "224bef3d88912ce68fa4a62175b91c02",
+    "url": "/static/media/待评价.224bef3d.svg"
+  },
+  {
+    "revision": "b67be111b984786bdb2a1d819d280862",
+    "url": "/static/media/烟.b67be111.svg"
+  },
+  {
+    "revision": "69caeb2444155237d8104778caf0a7b1",
+    "url": "/static/media/生活用品.69caeb24.svg"
+  },
+  {
+    "revision": "29047ce57f4e78b28aeb5430a83fecf0",
+    "url": "/static/media/粮油.29047ce5.svg"
+  },
+  {
+    "revision": "0800745f1054bb5727612a337968bc5c",
+    "url": "/static/media/调味品.0800745f.svg"
+  },
+  {
+    "revision": "9992f39c9f4e2f097932460b86ae2fa9",
+    "url": "/static/media/退换货.9992f39c.svg"
+  },
+  {
+    "revision": "56799b630588acd06086fb1046c806b7",
+    "url": "/static/media/酒.56799b63.svg"
+  },
+  {
+    "revision": "e6feee58642eccfb03d77038cb354442",
+    "url": "/static/media/零食.e6feee58.svg"
+  },
+  {
+    "revision": "9afd6bbe9dabaaea2adec81c9fddf1bf",
+    "url": "/static/media/饮料.9afd6bbe.svg"
+  }
+]);
